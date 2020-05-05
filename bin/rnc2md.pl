9d6c05618cd0a9764eb38012284830afdb7da126
Wolfram Diestel <diestel@steloj.de>
2020-05-05 20:24:17 +0200

    fin-formatigo de RNC kiel MD

--- bin/rnc2md.pl
+++ bin/rnc2md.pl
@@ -6,86 +6,137 @@ use warnings;
 my $basedir = $ENV{'VOKO'} || '.';
 my $rnc_file = $basedir."/dtd/vokoxml.rnc";
 
-open IN, $rnc_file
-    or die "Ne povis malfermi $rnc_file: $!";
 
 print "---\n";
 print "layout: page\n";
 print "title: Dokumentstrukturo (RelaxNG)\n";
 print "---\n";
 
-$_ = <IN>; print; print "\n";
+my $md;
+my @toc_hlp;
+my @toc_elm;
 
-# print "* Enhavo\n{:toc}\n\n";
+rnc2md(); 
 
-my $mode = 'header';
+print "![ulmobranĉo](../assets/img/ulmo.jpg){: style=\"float: right; margin-left: 2em; border: solid gray 1px\"}\n\n";
+print "# Enhavo\n\n";
+print "[⮊ Strukturo de artikolo](#strukturo-de-artikolo)  \n";
+print "[⮊ Strukturoj de detalaj elementoj](#strukturoj-de-detalaj-elementoj)\n\n";
 
-while (<IN>) {
+# eligu la elementojn
+print "**Strukturelementoj (rekte uzeblaj en XML):**  \n";
+for (sort @toc_elm) {
+    print "[$_](#$_-)&nbsp;| "
+}
+print "\n\n";
 
-    # ignoru liniojn kune nur unu komenca '#', ĉar tio estas verŝajne elkomentitaj linioj
-    if (m/^\s*#[^#]/) {
-        next;
-    }
+# eligu help-difinojn
+print "**Helpdifinoj (uzataj por difini la aranĝon de elementoj):**  \n";
+for (@toc_hlp) {
+    print "[$_](#$_-)&nbsp;| "
+}
+print "\n\n";
 
-    # ignoru liniojn antaŭ "namespace"
-    if ($mode eq 'header') {
-        if (m/^\s*namespace/) {
-            $mode = 'start'
-        }
-        next;
-    }
+# eligu la enhavon
+print $md;
 
-    if ($mode eq "start") {
-        if (m/^##\s*(?:\[([a-z\-]+)\]\s*)?(.*)$/) {
-            $mode = "desc";
-            print ("### $1\n\n") if ($1);
-            print $2; print "\n";
-        }
-        next;
-    }
 
-    if ($mode eq "header2") {
-        if (m/^##\s*\*+\s*$/) {
-            print "***\n";
-            $mode = "start";
-        } else {
-            print "##"; print; print "\n";
-        }
-        next;
-    }
+sub rnc2md {
+    open IN, $rnc_file
+        or die "Ne povis malfermi $rnc_file: $!";
 
-    if ($mode eq "desc") {
-        if (m/^##\s*(.*)$/) {
-            print $1;
-            print "\n";
-        } elsif (m/^[a-z\-]+\s*=/) {            
-            $mode = "def";
-            print "\n\n```\n";
-            print; 
-        }
-        next;
-    }
+    $_ = <IN>; print; print "\n";
 
-    if ($mode eq "def") {
-        if (m/^##\s*\*+\s*$/) {
-            print "```\n\n";
-            print "\n***\n  \n";
-            $mode = "header2";
+    # print "* Enhavo\n{:toc}\n\n";
+
+    my $mode = 'header';
+
+    while (<IN>) {
+
+        # ignoru liniojn kune nur unu komenca '#', ĉar tio estas verŝajne elkomentitaj linioj
+        if (m/^\s*#[^#]/) {
             next;
-        } elsif (m/^##\s*(?:\[([a-z\-]+)\]\s*)?(.*)$/) {
-            print "```\n\n";
-            $mode = "desc";
-            print ("### $1\n\n") if ($1);
-            print $2; print "\n";
-        } elsif (m/^\s*start\s*=/) {
-            print "```\n\n";
-            last;
-        } elsif (! m/^\s*$/) {
-            print;
         }
-        next;
+
+        # ignoru liniojn antaŭ "namespace"
+        if ($mode eq 'header') {
+            if (m/^\s*namespace/) {
+                $mode = 'start'
+            }
+            next;
+        }
+
+        if ($mode eq "start") {
+            if (m/^##\s*(?:\[([a-z_\-]+)\]\s*)?(.*)$/) {
+                $mode = "desc";
+                if ($1) {
+                    title($1);
+                }
+                $md .= "$2\n";
+            }
+            next;
+        }
+
+        if ($mode eq "header2") {
+            if (m/^##\s*\*+\s*$/) {
+                $md .= "***\n";
+                $mode = "start";
+            } elsif (m/^#+\s*(.*)\s*$/) {
+                $md .= "\n## *$1*{: style=\"color: brown\"}\n";
+            }
+            next;
+        }
+
+        if ($mode eq "desc") {
+            if (m/^##\s*(.*)$/) {
+                desc($1);
+            } elsif (m/^[a-z\-]+\s*=/) {            
+                $mode = "def";
+                $md .= "\n\n```\n$_";
+            }
+            next;
+        }
+
+        if ($mode eq "def") {
+            if (m/^##\s*\*+\s*$/) {
+                $md .= "```\n\n***\n.  \n";
+                $mode = "header2";
+                next;
+            } elsif (m/^##\s*(?:\[([a-z_\-]+)\]\s*)?(.*)$/) {
+                $md .= "```\n\n";
+                $mode = "desc";
+                if ($1) {
+                    title($1);
+                }
+                $md .= "$2\n";
+            } elsif (m/^\s*start\s*=/) {
+                $md .= "```\n\n";
+                last;
+            } elsif (! m/^\s*$/) {
+                $md .= $_;
+            }
+            next;
+        }
     }
 
+    close IN;
+}    
+
+sub desc {
+    my $line = shift;
+    $line =~s/<dfn>([^<]+)<\/dfn>/[$1](#$1-)/g;
+    $md .= "$line\n";
 }
 
-close IN;
+sub title {
+    my $entry = shift;
+
+    $md .= "### $entry";
+    $md .= " <a href=\"#enhavo\">⮉</a>";
+    $md .= "\n\n";
+    if ($entry =~ m/^_(.*)_/) {
+        push @toc_hlp, $1;
+    } else {
+        push @toc_elm, $entry;
+    }
+}
