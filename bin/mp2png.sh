4906ecc8bdb50589b5e15f28782a3f149b256f17
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-09-28 17:16:25 +0200

    mpost-PNG refunkcias, dum rsvg havas problemon eltondi formon el alia...

--- bin/mp2png.sh
+++ bin/mp2png.sh
@@ -3,18 +3,23 @@ set +e
 set +x
 
 MP=mpost
-RSVG=rsvg-convert
+
+# pro cimo en mpost-PNG 1.999 ni uzis rsvg-convert
+# sed nun rsvg-convert fuŝas kaj mpost-PNG denove funkcias!
+#RSVG=rsvg-convert
 
 cd smb
 
 for file in ???.mp ????.mp [ir]_*.mp
 do
-    svg=${file%%.mp}.svg
-    echo ${MP} ${file} ${svg}
-    ${MP} ${file} ${svg}
-    png=${file%%.mp}.png
-    echo ${RSVG} -w 100 -h 100 -o ${png} ${svg}
-    ${RSVG} -w 100 -h 100 -o ${png} ${svg}
+    #svg=${file%%.mp}.svg
+    #echo ${MP} ${file} ${svg}
+    #${MP} ${file} ${svg}
+    echo ${MP} ${file} 
+    ${MP} ${file} 
+    #png=${file%%.mp}.png
+    #echo ${RSVG} -w 100 -h 100 -o ${png} ${svg}
+    #${RSVG} -w 100 -h 100 -o ${png} ${svg}
 done
 
 cd ..
