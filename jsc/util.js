f61f62443976d2e7765420d5eeb4e6fedc8b803b
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-11-23 19:15:33 +0100

    kor. isIE, isKHTMLEdge

--- jsc/util.js
+++ jsc/util.js
@@ -172,7 +172,7 @@ function make_icon_button(iclass,handler,hint='') {
 function isIE() {
   ua = navigator.userAgent;
   /* MSIE used to detect old browsers and Trident used to newer ones*/
-  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+  var is_ie = ua.indexOf("MSIE") > -1 || ua.indexOf("Trident/") > -1;
   
   return is_ie; 
 }
@@ -180,7 +180,7 @@ function isIE() {
 function isKHTMLEdge() {
   ua = navigator.userAgent;
   /* MSIE used to detect old browsers and Trident used to newer ones*/
-  var is_ek = ua.indexOf("KHTML ") > -1 && ua.indexOf("Edge/") > -1;
+  var is_ek = ua.indexOf("KHTML") > -1 && ua.indexOf("Edge/") > -1;
   
   return is_ek; 
 }
