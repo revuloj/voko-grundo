2bec57c3556ab0bc259a749f8c775c91d86c68c6
Wolfram Diestel <diestel@steloj.de>
2021-02-10 20:43:49 +0100

    referencu al html/pl versio 1d

--- jsc/kadro.js
+++ jsc/kadro.js
@@ -3,11 +3,11 @@
 
 const debug=false; // ni bezonas provizore aparte por vidi erarojn en iOS Webkit, kie ni ne havas "console"
 const revo_url = "reta-vortaro.de";
-const sercho_url = "/cgi-bin/sercxu-json-1c.pl";
+const sercho_url = "/cgi-bin/sercxu-json-1d.pl";
 const hazarda_url = "/cgi-bin/hazarda_art.pl";
-const titolo_url = "titolo-1c.html";
-const redaktilo_url = "redaktilo-1c.html";
-const redaktmenu_url = "redaktmenu-1c.html";
+const titolo_url = "titolo-1d.html";
+const redaktilo_url = "redaktilo-1d.html";
+const redaktmenu_url = "redaktmenu-1d.html";
 //const inx_eo_url = "/revo/inx/_eo.html";
 
 const inx_eo_url = "/revo/inx/_plena.html";
