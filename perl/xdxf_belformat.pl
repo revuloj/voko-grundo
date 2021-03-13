5e0fc1336f15d44319b57442f72b532fed1e5a5a
Wolfram Diestel <diestel@steloj.de>
2021-03-13 18:16:46 +0100

    komentoj

--- perl/xdxf_belformat.pl
+++ perl/xdxf_belformat.pl
@@ -15,18 +15,23 @@ open OUT,">$outfile"
 
 while(<IN>) {
     unless( /\s*<\/ar>\s*/ ) {
+        # ni kolektas liniojn ĝis </ar> kaj anstataŭigas linifinojn per spacoj
         chomp;
         $art .= ' ' if $art ne '';
         $art .= $_;
     }
     else {
         $art .= $_;
+        # kun, kiam ni kolektis tutan artikolon, ni normaligas ĝin
+        # forigante duoblajn spacojn...
         $art =~ s/\s+/ /g;
-
+        # aldonante linirompjan ĉe XML-strukturiloj samtempe forigante ĉirkaŭajn spacojn
         $art =~ s/ *<(def|sr|)> */\n\n<$1>/g;
-	$art =~ s/ *<(\/def)> */<$1>\n/g;
-	$art =~ s/ *<(\/ar)> */<$1>\n\n/g;
+	    $art =~ s/ *<(\/def)> */<$1>\n/g;
+	    $art =~ s/ *<(\/ar)> */<$1>\n\n/g;
+        # aldonante punkton komence de ĉiu ekzemplo
         $art =~ s/ *(<ex\b[^>]*>) */\n$1•  /g;
+        # aldonate sagon antaŭ referencoj
         $art =~ s/ *<sr> */\n<sr>► /g;
         $art =~ s/ *<dtrn> */\n<dtrn> /g;
 
