de5871d22e0c09824c43cb422a25e0ce04d05581
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-04-28 16:42:37 +0200

    1f

--- bin/eldonu-kadraron.sh
+++ bin/eldonu-kadraron.sh
@@ -11,15 +11,15 @@
 #host=retavortaro.de
 # aldonu en /etc/hosts!
 host=revo
-release=1e
+release=1f
 revo=${host}:www/revo
 files=${host}:files
 
 # poste la plusendan index.html ni havu ankaŭ rekte sub /revo...
 #scp -r build/* ${revo}/
 
-scp -r build/jsc/revo-${release}-min.js ${revo}/jsc
-scp -r build/stl/revo-${release}-min.css ${revo}/stl
+#scp -r build/jsc/revo-${release}-min.js ${revo}/jsc
+#scp -r build/stl/revo-${release}-min.css ${revo}/stl
 
 ## scp -r cfg/*.xml ${revo}/cfg/
 
