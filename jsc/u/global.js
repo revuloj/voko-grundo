0b6012c12bc35fa5f8b5dbd67af41d3147d963dd
Wolfram Diestel <diestel@steloj.de>
2021-11-21 10:07:49 +0100

    doc

--- jsc/u/global.js
+++ jsc/u/global.js
@@ -1,6 +1,8 @@
-// Por eviti problemojn kun enpakado de moduloj en google-closure-compiler
-// ni kreas tie ĉi nomspacon por globaj variabloj
-
+/**
+ * Por eviti problemojn kun enpakado de moduloj (konkrete en google-closure-compiler)
+ * ni kreas tie ĉi nomspacon por globaj variabloj. Vi povas aldoni pliajn simple kiel
+ * variablo aŭ konstanto kaj aliri ilin per global.mia_variablo
+ */
 var global = function() {
     var preflng='en';
 }
