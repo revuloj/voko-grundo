b3545efb0819867684c68f1cd431fddf51c631c3
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-12-01 10:23:37 +0100

    +komento

--- jsc/x/xmlstruct.js
+++ jsc/x/xmlstruct.js
@@ -111,17 +111,22 @@ XmlStruct.prototype.structure = function(selected = undefined) {
   function id(subt) {
     const rx = /[^A-Za-z]/g;
     const key = [111,222,33,44]; // ne tro gravas...
-    var xor_str = function(str)
-    { 
-        var c = key;
-        for(let i=0; i<str.length; i++) { 
-            c[i%key.length] ^= str.charCodeAt(i);
-        }
-        return c.join('.');
-    };
+    const xor_str = (str) => // kondensigi signoĉenon al cifera identigilo
+      { 
+          var c = key;
+          for(let i=0; i<str.length; i++) { 
+              c[i%key.length] ^= str.charCodeAt(i);
+          }
+          return c.join('.');
+      };
     if (subt.mrk) {
+      // se la elemento havas markon, tio estas la plej bona identigilo
       return xor_str(subt.mrk);
     } else {
+      // se ne, ni uzas la unuajn aperantajn latinajn literojn por
+      // identigi, ja konsciante, ke tiuj povos ŝanĝiĝi, sed tiam
+      // ni rekalkulas la strukturon kaj akceptas, ke ni ne
+      // retrovas la antaŭan elekton...
       return xor_str(xmlteksto.substr(subt.de,120).replace(rx,''));
     }
   }
