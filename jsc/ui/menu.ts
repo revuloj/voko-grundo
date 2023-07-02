5e7a887c11d2b6531a6342ece1eb8cd08f09d1f7
Wolfram Diestel <diestel@steloj.de>
2023-07-02 17:57:11 +0200

    trakto de submenuoj (traduklingvoj)

--- jsc/ui/menu.ts
+++ jsc/ui/menu.ts
@@ -3,11 +3,14 @@
  * laŭ GPL 2.0
  */
 
+import { DOM } from './dom';
 import { UIElement } from './uielement';
 
 export class Menu extends UIElement {
     //valoroj: any;
     static menu_item_class = "ui-menu-item";
+    static menu_class = "ui-menu";
+    static menu_sub_fermita_class = "ui-menu-sub-fermita";
 
     static _default: {
         eroj: "li",
@@ -15,7 +18,7 @@ export class Menu extends UIElement {
      }
 
     static menu(element: HTMLElement|string) {
-        const m = super.obj(element);
+        const m = super.obj(element);        
         if (m instanceof Menu) return m;
     }
 
@@ -27,7 +30,8 @@ export class Menu extends UIElement {
     constructor(element: HTMLElement|string, opcioj: any) {
         super(element, opcioj);
 
-        this.opcioj = Object.assign(this.opcioj,Menu._default,opcioj)
+        this.opcioj = Object.assign(this.opcioj,Menu._default,opcioj);
+        this.element.classList.add(Menu.menu_class);
 
         // preparu la menuerojn
         this._preparu();
@@ -36,7 +40,20 @@ export class Menu extends UIElement {
     _preparu() {
         this.element.querySelectorAll(this.opcioj.eroj).forEach((i) => {
             i.classList.add(Menu.menu_item_class);
-            i.addEventListener("click",this.opcioj.reago);
+
+            // traktu evtl. submenuon            
+            const sub = i.querySelector("ul,ol");
+            if (sub) {
+                i.classList.add(Menu.menu_sub_fermita_class);
+                DOM.kaŝu(sub);
+                i.addEventListener("click",(event)=> {
+                    const kaŝita = DOM.kaŝita(sub);
+                    DOM.kaŝu(sub,!kaŝita);
+                });
+            } else {
+                i.addEventListener("click",this.opcioj.reago);
+            }
+
         });
     }
 
