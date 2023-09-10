58bde360303faf7b674ad0916ad53231bd3f175e
Wolfram Diestel <diestel@steloj.de>
2023-09-10 18:33:01 +0200

    +kor. por ĉinaj transskriboj

--- perl/replace_in_art.pl
+++ perl/replace_in_art.pl
@@ -8,7 +8,7 @@
 # vi devas ŝanĝi tie!
 
 #
-#  perl replace_in_art.pl a*.xml
+#  perl replace_in_art.pl revo/a*.xml
 
 use lib("../voko-grundo/perl");
 use process;
@@ -20,7 +20,7 @@ $debug = 0;
 $verbose=1;
 @artikoloj = @ARGV;
 
-my $shanghoj = "revo: aktualigo URL-unuoj";
+my $shanghoj = "revo: korekto de transskribo";
 
 for $art (@artikoloj) {
     process_art($art);
@@ -34,30 +34,38 @@ sub process_art {
     my $chg = 0;
     
     # apliku anstataŭigojn
-    $chg += ($xml =~ s|https?://(?:www\.)?monato\.be|&Monato;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?monato\.net|&Monato;|g);
-    $chg += ($xml =~ s|https?://esperanto\.cri\.cn/|&CRI;|g);
-    $chg += ($xml =~ s|https?://eo\.mondediplo\.com/|&Mondediplo;|g);
-    $chg += ($xml =~ s|https?://eo\.wikipedia\.org/w/index.php\?title=|&Vikio;|g);
-    $chg += ($xml =~ s|https?://eo\.wikipedia\.org/wiki|&Viki;|g);
-    $chg += ($xml =~ s,https?://groups\.google\.(?:com|be)/(?:g|group)/soc\.culture\.esperanto\?hl=eo,&SCE;,g);
-    $chg += ($xml =~ s|http://(?:www\.)?kono\.be/cgi-bin/vivo/ViVo.cgi\?pagxo=|&ViVoTrd;|g);
-    $chg += ($xml =~ s|http://(?:www\.)?kono\.be/cgi-bin/vivo/|&ViVo;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?akademio-de-esperanto\.org|&AdE;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?bertilow\.com/pmeg/|&PMEG;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?monato\.be|&Monato;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?monato\.net|&Monato;|g);
+#    $chg += ($xml =~ s|https?://esperanto\.cri\.cn/|&CRI;|g);
+#    $chg += ($xml =~ s|https?://eo\.mondediplo\.com/|&Mondediplo;|g);
+#    $chg += ($xml =~ s|https?://eo\.wikipedia\.org/w/index.php\?title=|&Vikio;|g);
+#    $chg += ($xml =~ s|https?://eo\.wikipedia\.org/wiki|&Viki;|g);
+#    $chg += ($xml =~ s,https?://groups\.google\.(?:com|be)/(?:g|group)/soc\.culture\.esperanto\?hl=eo,&SCE;,g);
+#    $chg += ($xml =~ s|http://(?:www\.)?kono\.be/cgi-bin/vivo/ViVo.cgi\?pagxo=|&ViVoTrd;|g);
+#    $chg += ($xml =~ s|http://(?:www\.)?kono\.be/cgi-bin/vivo/|&ViVo;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?akademio-de-esperanto\.org|&AdE;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?bertilow\.com/pmeg/|&PMEG;|g);
+#
+#    $chg += ($xml =~ s|https?://(?:www\.)?eventoj\.hu/steb/|&STEB;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?literaturo\.org/HARLOW-Don/Esperanto/Literaturo/|&DonH;|g);
+#    $chg += ($xml =~ s|https?://eduinf\.waw\.pl/esp/|&EduInf;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?esperanto-ondo\.ru/|&LOdE;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?sezonoj\.ru/|&Sezonoj;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?gutenberg\.org/|&Gutenberg;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?steloj\.de/esperanto/|&Steloj;|g);
+#    $chg += ($xml =~ s|https?://upload\.wikimedia\.org/wikipedia/commons|&WCU;|g);
+#    $chg += ($xml =~ s|https?://commons\.wikimedia\.org/wiki|&WCW;|g);
+#
+#    $chg += ($xml =~ s|https?://(?:www\.)?uea\.org/|&UEA;|g);
+#    $chg += ($xml =~ s|https?://(?:www\.)?tekstaro\.com/t|&Tekstaro;|g);
 
-    $chg += ($xml =~ s|https?://(?:www\.)?eventoj\.hu/steb/|&STEB;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?literaturo\.org/HARLOW-Don/Esperanto/Literaturo/|&DonH;|g);
-    $chg += ($xml =~ s|https?://eduinf\.waw\.pl/esp/|&EduInf;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?esperanto-ondo\.ru/|&LOdE;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?sezonoj\.ru/|&Sezonoj;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?gutenberg\.org/|&Gutenberg;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?steloj\.de/esperanto/|&Steloj;|g);
-    $chg += ($xml =~ s|https?://upload\.wikimedia\.org/wikipedia/commons|&WCU;|g);
-    $chg += ($xml =~ s|https?://commons\.wikimedia\.org/wiki|&WCW;|g);
+    # korektu malĝustajn literojn en ĉina transskribo pinjina
+    $chg += ($xml =~ s,<ind>(.*?)(?:&#x15FB;|&#5627;)(.*?)</ind>,<ind>$1ī$2</ind>,g); # ᗻ
+    $chg += ($xml =~ s|<ind>(.*?)(?:&#x15E3;)(.*?)</ind>|<ind>$1ē$2</ind>|g); # ᗣ
+    $chg += ($xml =~ s|<ind>(.*?)(?:&#x15D1;)(.*?)</ind>|<ind>$1ā$2</ind>|g); # ᗑ
+    $chg += ($xml =~ s|<ind>(.*?)(?:&#x163B;)(.*?)</ind>|<ind>$1ū$2</ind>|g); # ᘻ
+    $chg += ($xml =~ s|<ind>(.*?)(?:&#x161D;)(.*?)</ind>|<ind>$1ō$2</ind>|g); # ᘝ
 
-    $chg += ($xml =~ s|https?://(?:www\.)?uea\.org/|&UEA;|g);
-    $chg += ($xml =~ s|https?://(?:www\.)?tekstaro\.com/t|&Tekstaro;|g);
 
     if ($chg) {
         process::write_file(">",$art,$xml);
